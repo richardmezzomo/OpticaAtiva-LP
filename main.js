3a4566871dc4387d764d31d24eb433041e2b63fe
richardmezzomo <richardmezzomo1@gmail.com>
2023-04-29 10:04:24 -0300

    refactor: mobile

--- main.js
+++ main.js
@@ -1,49 +1,49 @@
-// window.addEventListener('scroll', onScroll)
+window.addEventListener('scroll', onScroll)
 
-// onScroll()
-// function onScroll() {
-//   // showNavOnScroll()
-//   showBackToTopButtonOnScroll()
+onScroll()
+function onScroll() {
+  // showNavOnScroll()
+  showBackToTopButtonOnScroll()
 
-//   activateMenuAtCurrentSection(home)
-//   activateMenuAtCurrentSection(services)
-//   activateMenuAtCurrentSection(about)
-//   activateMenuAtCurrentSection(contact)
-// }
+  activateMenuAtCurrentSection(home)
+  activateMenuAtCurrentSection(services)
+  activateMenuAtCurrentSection(about)
+  activateMenuAtCurrentSection(contact)
+}
 
-// function activateMenuAtCurrentSection(section) {
-//   const targetLine = scrollY + innerHeight / 2
-
-
-//   const sectionTop = section.offsetTop
-//   const sectionHeight = section.offsetHeight
-//   const sectionTopReachOrPassedTargetline = targetLine >= sectionTop
+function activateMenuAtCurrentSection(section) {
+  const targetLine = scrollY + innerHeight / 2
 
 
+  const sectionTop = section.offsetTop
+  const sectionHeight = section.offsetHeight
+  const sectionTopReachOrPassedTargetline = targetLine >= sectionTop
 
-//   const sectionEndsAt = sectionTop + sectionHeight
-//   const sectionEndPassedTargetline = sectionEndsAt <= targetLine
+
+
+  const sectionEndsAt = sectionTop + sectionHeight
+  const sectionEndPassedTargetline = sectionEndsAt <= targetLine
 
  
-//   const sectionBoundaries =
-//     sectionTopReachOrPassedTargetline && !sectionEndPassedTargetline
+  const sectionBoundaries =
+    sectionTopReachOrPassedTargetline && !sectionEndPassedTargetline
 
-//   const sectionId = section.getAttribute('id')
-//   const menuElement = document.querySelector(`.menu a[href*=${sectionId}]`)
+  const sectionId = section.getAttribute('id')
+  const menuElement = document.querySelector(`.menu a[href*=${sectionId}]`)
 
-//   menuElement.classList.remove('active')
-//   if (sectionBoundaries) {
-//     menuElement.classList.add('active')
-//   }
-// }
+  menuElement.classList.remove('active')
+  if (sectionBoundaries) {
+    menuElement.classList.add('active')
+  }
+}
 
-// function showNavOnScroll() {
-//   if (scrollY > 0) {
-//     navigation.classList.add('scroll')
-//   } else {
-//     navigation.classList.remove('scroll')
-//   }
-// }
+function showNavOnScroll() {
+  if (scrollY > 0) {
+    navigation.classList.add('scroll')
+  } else {
+    navigation.classList.remove('scroll')
+  }
+}
 
 function showBackToTopButtonOnScroll() {
   if (scrollY > 550) {
