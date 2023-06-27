b094307d1510d9f007442a57acbedddbfdc68259
richardmezzomo <richardmezzomo1@gmail.com>
2023-06-27 16:03:25 -0300

    v1.0.1

---
+++ README.md
@@ -0,0 +1,17 @@
+
+# Landing Page Óptica Ativa
+
+Esse projeto é uma landing page criada para uma óptica, ou seja, é uma página da web projetada para ajudar a empresa a promover seus produtos e serviços online. A landing page apresenta informações sobre os produtos e serviços oferecidos pela óptica, como óculos de grau, óculos de sol, lentes de contato, serviços de ajuste e reparo de óculos.
+
+O objetivo dessa landing page é atrair clientes em potencial e fornecer informações úteis e relevantes sobre os produtos e serviços da óptica. 
+
+A página é projetada para ser atraente e fácil de navegar e que possa direcionar o cliente para aonde ele quer com informações claras e concisas sobre os serviços oferecidos pela óptica.
+
+
+## Autores
+
+- [@richardmezzomo](https://www.github.com/richardmezzomo)
+
+## Design 
+
+- [@endieguedes](https://www.linkedin.com/in/endieguedes/)
---
+++ main.js
@@ -0,0 +1,114 @@
+window.addEventListener('scroll', onScroll)
+
+onScroll()
+function onScroll() {
+  // showNavOnScroll()
+  showBackToTopButtonOnScroll()
+
+  activateMenuAtCurrentSection(home)
+  activateMenuAtCurrentSection(services)
+  activateMenuAtCurrentSection(about)
+  activateMenuAtCurrentSection(contact)
+}
+
+function activateMenuAtCurrentSection(section) {
+  const targetLine = scrollY + innerHeight / 2
+
+
+  const sectionTop = section.offsetTop
+  const sectionHeight = section.offsetHeight
+  const sectionTopReachOrPassedTargetline = targetLine >= sectionTop
+
+
+
+  const sectionEndsAt = sectionTop + sectionHeight
+  const sectionEndPassedTargetline = sectionEndsAt <= targetLine
+
+ 
+  const sectionBoundaries =
+    sectionTopReachOrPassedTargetline && !sectionEndPassedTargetline
+
+  const sectionId = section.getAttribute('id')
+  const menuElement = document.querySelector(`.menu a[href*=${sectionId}]`)
+
+  menuElement.classList.remove('active')
+  if (sectionBoundaries) {
+    menuElement.classList.add('active')
+  }
+}
+
+function showNavOnScroll() {
+  if (scrollY > 0) {
+    navigation.classList.add('scroll')
+  } else {
+    navigation.classList.remove('scroll')
+  }
+}
+
+function showBackToTopButtonOnScroll() {
+  if (scrollY > 550) {
+    backToTopButton.classList.add('show')
+  } else {
+    backToTopButton.classList.remove('show')
+  }
+}
+
+function openMenu() {
+  document.body.classList.add('menu-expanded')
+}
+
+function closeMenu() {
+  document.body.classList.remove('menu-expanded')
+}
+
+ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 700
+}).reveal(`
+  #home, 
+  #home img, 
+  #home, 
+  #brands,
+  #faceter header,
+  #faceter .texts,
+  #faceter .img-faceter,
+  #services,
+  #services header,
+  #services .cards
+  #about, 
+  #about header, 
+  #about .content`)
+
+const swiper = new Swiper(".mySwiper", {
+  slidesPerView: 4,
+  freeMode: true,
+  width: 500,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
+
+const swiperModels = new Swiper(".mySwiperModels", {
+  slidesPerView: 4,
+  freeMode: true,
+  loop: true,
+  width: 600,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+});
+
+const swiperDesktop = new Swiper(".mySwiper-desktop", {
+  slidesPerView: 6,
+  freeMode: true,
+  width: 800,
+  loop: true,
+});
